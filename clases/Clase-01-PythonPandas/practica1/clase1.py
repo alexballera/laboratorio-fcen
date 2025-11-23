34c0a0fefc0079a5b725edf25df57f818c1e9b8f
alexballera <alexballera@gmail.com>
2025-11-22 22:12:55 -0300

    feat: agregar funciones para simular tiradas de dados y verificar opciones, y crear archivo filtrado de estudiantes

--- clases/Clase-01-PythonPandas/practica1/clase1.py
+++ clases/Clase-01-PythonPandas/practica1/clase1.py
@@ -6,6 +6,7 @@
 # %% importar librerías
 import math
 import csv
+import random
 # %% Copias
 a = [1, 2, 3, 4, 5]
 b = a  # Copia por referencia
@@ -64,7 +65,76 @@ f = open(ruta + nombre_archivo, 'rt')
 lineas = csv.reader(f)
 encabezado = next(f)
 print('Encabezado', encabezado)
+
 for linea in lineas:
     print(linea)
 f.close()
+# %% Ejercicio 1
+'''
+Escribir una función generala_tirar()  que simule una tirada de dados
+para el juego de la generala. Es decir, debe devolver una lista aleatoria
+de 5 valores de dados. Por ejemplo [2,3,2,1,6]
+'''
+# %% tirar dados
+def tirar_dados():
+    return [random.randint(1,6) for i in range(1,6)]
+
+tirar_dados()    
+# %% escalera
+def escalera(numeros):
+    return numeros == [1,2,3,4,5] or numeros == [2,3,4,5,6]
+# %% verificar opciones
+def verificar_opciones(numeros):
+    num = sorted(numeros)
+    if (num[0] == num[-1]):
+        print('Generala')
+    
+    elif (num[0] == num[-2]):
+        print('Poker')
+        
+    elif (num[-1] == num[-2]):
+        print('Full')
+    
+    elif escalera(numeros):
+        print('Escalera')
+    
+    else:
+        print('Ninguno')
+
+    return numeros
+    
+verificar_opciones(tirar_dados())
+# %% datame.txt
+'''
+Escribir un programa que recorra las líneas del archivo ‘datame.txt’
+e imprima solamente las líneas que contienen la palabra ‘estudiante’
+'''
+path = './'
+name = 'datame.txt'
+file = open(path + name, 'rt')
+
+data = [line for line in file.readlines() if 'estudiante' in line]
+
+datame_estudiante = ''
+for line in data:
+    datame_estudiante += line
+
+nuevo_doc = open('datame_estudiante.txt', 'w')
+nuevo_doc.write(datame_estudiante)
+nuevo_doc.close()
+
+# %%
+'''
+Utilizando el archivo cronograma_sugerido , armar una lista de las
+materias del cronograma, llamada “lista_materias 
+'''
+nombre_archivo = 'cronograma_sugerido.csv'
+# %% “cuantas_materias(n)”
+'''
+Luego, definir una función “cuantas_materias (n)” que, dado un
+número de cuatrimestre (n entre 3 y 8), devuelva la cantidad de
+materias a cursar en ese cuatrimestre. Por ejemplo:
+cuantas_materias(5) debe devolver 3.
+'''
+
 # %%
