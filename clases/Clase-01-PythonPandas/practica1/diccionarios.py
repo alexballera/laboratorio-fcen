61bdfe6811a05e756dd0d68df8d0d8c412f2a0d9
alexballera <alexballera@gmail.com>
2025-11-23 15:56:30 -0300

    feat: eliminar archivos temporales y agregar nuevos scripts de pandas y diccionarios

--- clases/Clase-01-PythonPandas/practica1/diccionarios.py
+++ clases/Clase-01-PythonPandas/practica1/diccionarios.py
@@ -4,7 +4,7 @@ Editor de Spyder
 
 Este es un archivo temporal.
 """
-
+# %%
 feriados = {}
 feriados[(1,1)] = 'Año nuevo'
 feriados[(1,5)] = 'Día del trabajador'
@@ -19,3 +19,4 @@ for t in zip([1,2,3,4], [5,6,7,8]):
 
 cuadrados2 = dict(zip([1,2,3,4], [5,6,7,8]))
 print(cuadrados2[1])
+# %%
