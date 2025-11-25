5423c1e4f73904e1083ac1292c637d9196b28511
alexballera <alexballera@gmail.com>
2025-11-25 09:15:47 -0300

    feat: update leer_parque function to return multiple data structures and remove unused file

--- clases/Clase-01-PythonPandas/practica/practica1.py
+++ clases/Clase-01-PythonPandas/practica/practica1.py
@@ -8,22 +8,41 @@ import numpy as np
 import pandas as pd
 # %% Ejercicio 1
 def leer_parque(nombre_archivo, parque):
-    # creo DataFrame
     path = '/home/alexballera/Documents/uba/laboratorio-fcen/clases/Clase-01-PythonPandas/practica/'
     archivo = nombre_archivo
     df = pd.read_csv(path + archivo)
 
     arboles_df = df[df['espacio_ve'] == parque]
-
-    df_grouped = arboles_df.groupby('nombre_cie')
     
-    #for item in df_grouped.iterrows():
-     #   print(item)
+    # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_dict.html
+    dict = arboles_df.to_dict(orient='records')
+    
+    lista = []
+    for _, row in arboles_df.iterrows():
+        # construir dict usando las columnas y los valores de la fila
+        registro = {col: row[col] for col in arboles_df.columns}
+        lista.append(registro)
+    
+    columnas = arboles_df.columns
+    lista2 = []
+    arbol = {}
+    i = 0
+    
+    for _, row in arboles_df.iterrows():
+        for item in row:
+            arbol[columnas[i]] = item
+            i += 1
+        arbol_copy = arbol.copy()
+        lista2.append(arbol_copy)
+        i = 0
 
-    return df_grouped, arboles_df
-nombre_archivo = 'arbolado-en-espacios-verdes.csv'
-parque = 'GENERAL PAZ'
-df_grouped = leer_parque(nombre_archivo, parque)[0]
-arboles_df = leer_parque(nombre_archivo, parque)[1]
+    return dict, lista, lista2
+
+
+if __name__ == '__main__':
+    nombre_archivo = 'arbolado-en-espacios-verdes.csv'
+    parque = 'GENERAL PAZ'
+    dict, lista, lista2 = leer_parque(nombre_archivo, parque)
+    print(f"Árboles en {parque}: {len(lista)}")
 
 # %%
