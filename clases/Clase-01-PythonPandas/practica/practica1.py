b84dcc2d5f97460c2bc7dfadcf8edc12363771bf
alexballera <alexballera@gmail.com>
2025-11-26 15:52:20 -0300

    refactor: enhance comments in leer_parque function and organize code structure

--- clases/Clase-01-PythonPandas/practica/practica1.py
+++ clases/Clase-01-PythonPandas/practica/practica1.py
@@ -12,18 +12,19 @@ def leer_parque(path, nombre_archivo, parque):
 
     arboles_df = df[df['espacio_ve'] == parque]
     
-    # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_dict.html
+    # 3 formas de crear una lista de diccionarios a partir de un DataFrame de pandas
+    # 1. https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_dict.html
     lista1 = arboles_df.to_dict(orient='records')
     
     columnas = arboles_df.columns
     lista2 = []
     
-    # construir dict usando las columnas y los valores de la fila
+    # 2. construir dict usando las columnas y los valores de la fila
     for _, row in arboles_df.iterrows():
         registro = {col: row[col] for col in columnas}
         lista2.append(registro)
     
-    # construir dict usando las columnas y los valores de la fila
+    # 3. construir dict usando las columnas y los valores de la fila
     lista3 = []
     i = 0
     
@@ -34,16 +35,43 @@ def leer_parque(path, nombre_archivo, parque):
             i += 1
         lista3.append(registro)
         i = 0
-
+    
     return lista1, lista2, lista3
 
-# %%
+# %% Ejercicio 2
+def especies(lista_arboles):
+    arboles = set()
+    
+    arboles = {arbol['nombre_com'] for arbol in lista_arboles}
 
+    return arboles
+# %% Ejercicio 3
+
+# %% Ejercicio 4
+
+# %% Ejercicio 5
+
+# %% Ejercicio 6
+
+# %% main
 if __name__ == '__main__':
+    # Ejercicio 1
     path = '/home/alexballera/Documents/uba/laboratorio-fcen/clases/Clase-01-PythonPandas/practica/'
     nombre_archivo = 'arbolado-en-espacios-verdes.csv'
     parque = 'GENERAL PAZ'
     lista1, lista2, lista3 = leer_parque(path, nombre_archivo, parque)
     print(f"Árboles en {parque}: {len(lista1)}")
+    
+    # Ejercicio 2
+    arboles = especies(lista1)
+    print(f"Especies en {parque}: {len(arboles)}")
+    
+    # Ejercicio 3
+    
+    # Ejercicio 4
+    
+    # Ejercicio 5
+    
+    # Ejercicio 6
 
 # %%
